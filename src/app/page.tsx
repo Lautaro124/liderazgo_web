aed27d253d12258be840c42614159c3357fd38a6
Lautaro Gonzalez <59624470+Lautaro124@users.noreply.github.com>
2025-03-22 20:43:00 -0300

    Remember

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,36 @@ import { saveUser } from "@/redux/slice/user.slice";
 import { loginAction } from "@/service/authentication/login.service";
 import { Mail, Lock } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const route = useRouter();
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user);
+  const [rememberMe, setRememberMe] = useState(false);
 
   const handleSubmit = async (formData: FormData) => {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
+
+    
     const response = await loginAction({ email, password });
     if (response) {
+      if (rememberMe) {
+        localStorage.setItem("rememberedEmail", email);
+      } else {
+        localStorage.removeItem("rememberedEmail");
+      }
       dispatch(saveUser(response));
     }
   };
+
+  useEffect(() => {
+    const savedEmail = localStorage.getItem("rememberedEmail");
+    if (savedEmail) {
+      setRememberMe(true);
+    }
+  }, []);
 
   useEffect(() => {
     if (user.fullName !== "") {
@@ -46,6 +61,7 @@ export default function Home() {
             type="email"
             autoComplete="email"
             required
+            defaultValue={localStorage.getItem("rememberedEmail") || ""}
             icon={<Mail className="h-5 w-5 text-gray-400" />}
           />
           <InputField
@@ -57,6 +73,22 @@ export default function Home() {
             required
             icon={<Lock className="h-5 w-5 text-gray-400" />}
           />
+          <div className="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+              className="h-4 w-4 text-purple-600 focus:ring-purple-500 border-gray-300 rounded"
+            />
+            <label
+              htmlFor="remember-me"
+              className="ml-2 block text-sm text-gray-900"
+            >
+              Recordarme
+            </label>
+          </div>
           <Button type="submit">Iniciar sesion</Button>
         </Form>
         {/* <SingRedirect
